42d2b155ae34d63b5a3aea6d242ec9a0d2e0ddce
jessekornblum <jessekornblum@d1b3761f-3242-0410-b1a5-fe525d664610>
2011-09-30 10:54:30 +0000

    Send errors to stderr
    git-svn-id: https://svn.code.sf.net/p/ssdeep/code/branches/stdin@116 d1b3761f-3242-0410-b1a5-fe525d664610

--- ui.c
+++ ui.c
@@ -27,10 +27,10 @@ void print_error(state *s, char *fmt, ...)
   va_list(ap);
   
   va_start(ap,fmt); 
-  vprintf(fmt,ap); 
+  vfprintf(stderr,fmt,ap); 
   va_end(ap); 
   
-  printf ("%s", NEWLINE);
+  fprintf (stderr,"%s", NEWLINE);
 
 }
 

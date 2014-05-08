59f34ae8f890b0a31fe6b32b6e5cd4fdef8fb9af
jessekornblum <jessekornblum@d1b3761f-3242-0410-b1a5-fe525d664610>
2014-05-08 23:04:56 +0000

    Updates for FC20
    git-svn-id: https://svn.code.sf.net/p/ssdeep/code/trunk@210 d1b3761f-3242-0410-b1a5-fe525d664610

--- tchar-local.h
+++ tchar-local.h
@@ -8,9 +8,6 @@
 /* Unicode support */
 #ifdef _WIN32
 
-// This says that we require Windows NT 4.0 to run
-#define _WIN32_WINNT 0x0400
-
 # include <windows.h>
 # include <wchar.h>
 # include <tchar.h>

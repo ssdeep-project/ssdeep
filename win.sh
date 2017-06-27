e48f3907b27f2a74b147d60ab482735b92eae6f8
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2017-06-27 17:25:10 +0900

    make sure that libwinpthread is statically linked
    Recent versions of mingw-w64 dynamically links `libwinpthread-1.dll`
    by default. This commit resolves this issue.
    
    *	Not to set `LIBS` on `configure.ac`
    	setting `LIBS` on `configure.ac` prevents proper configuration.
    *	To set `LIBS` on `win.sh` and `Makefile.am` (`make world`)
    	to make sure that `libwinpthread` is statically linked on mingw-w64.

--- win.sh
+++ win.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 make distclean
-./configure --host=i686-w64-mingw32 CXXFLAGS="-Wall -W -g -ggdb -O0 -static" CFLAGS="-Wall -W -g -ggdb -O0 -static"
+./configure --host=i686-w64-mingw32 CXXFLAGS="-Wall -W -g -ggdb -O0 -static" CFLAGS="-Wall -W -g -ggdb -O0 -static" LIBS="-Wl,-Bstatic -lstdc++ -lpthread -static-libgcc"
 make

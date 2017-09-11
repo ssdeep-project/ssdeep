aeb9c4438adc1982c4ca2b4a26931906026c7105
Tsukasa OI <floss_ssdeep@irq.a4lg.com>
2017-09-11 13:13:12 +0900

    Make Windows binaries optimized

--- win.sh
+++ win.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 make distclean
-./configure --host=i686-w64-mingw32 CXXFLAGS="-Wall -W -g -ggdb -O0 -static" CFLAGS="-Wall -W -g -ggdb -O0 -static" LIBS="-Wl,-Bstatic -lstdc++ -lpthread -static-libgcc"
+./configure --host=i686-w64-mingw32 CXXFLAGS="-Wall -W -g -ggdb -O3 -static" CFLAGS="-Wall -W -g -ggdb -O3 -static" LIBS="-Wl,-Bstatic -lstdc++ -lpthread -static-libgcc"
 make
